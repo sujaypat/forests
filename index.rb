58c0b4b75cb6245d8518cc173c803ed30100efea
Pranay Prakash <pranay.gp@gmail.com>
2015-09-12 10:38:51 -0400

    Expanded API to get data from different years and data sources

--- index.rb
+++ index.rb
@@ -2,38 +2,35 @@ require 'sinatra'
 require 'aws-sdk-core'
 require 'json'
 
-get "/index?.:format?" do
+get "/:year/:ticker?.:format?" do
 	dynamodb = Aws::DynamoDB::Client.new(
 		region: 'us-east-1',
 		credentials: Aws::Credentials.new('AKIAI6PU7IIGNUBSI26A', 'qBrlCJMywo029GlaXsLVI8Rf4286eWsC6aU+ue53')
 )
 	@data = dynamodb.query({
-		:table_name => '2012', 
+		:table_name => params["year"], 
 		:consistent_read => true,
 		:select => 'ALL_ATTRIBUTES',
 		:key_conditions => { 
 		  'Ticker' => { 
 		    :comparison_operator => 'EQ', 
 		    :attribute_value_list => [
-		      "ACRDBT Index"
+		      params["ticker"] + " Index"
 		    ],
 		  },
 		  'Date' => { 
 		    :comparison_operator => 'BETWEEN', 
 		    :attribute_value_list => [
-		      '2012-01-01', '2012-12-31'
+		      params["year"]+'-01-01', params["year"]+'-12-31'
 		    ]
 		  } 
 		}
 	})
-
-	@debug = dynamodb.describe_table ({:table_name => '2012'})
-	# @data = [{:time => 0, :value => 10}]
-	if params['format'] == "html"
-		erb :index 
-	elsif params['format'] == "json"
+	if params['format'] == "json"
 		content_type :json
 		@data.items.to_json
+	else
+		erb :Index
 	end
 		
 end
