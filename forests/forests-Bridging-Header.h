f495f3f4cb502bb9bc1c78fc6e142aa5f44ef878
Sujay <cheeselover724@gmail.com>
2015-09-12 14:58:45 -0400

    building

--- forests/forests-Bridging-Header.h
+++ forests/forests-Bridging-Header.h
@@ -1,19 +1,7 @@
 //
-//  forests-Bridging-Header.h
-//  forests
+//  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-//  Created by Sujay Patwardhan on 9/12/15.
-//  Copyright © 2015 Sujay Patwardhan. All rights reserved.
-//
-
-#ifndef forests_Bridging_Header_h
-#define forests_Bridging_Header_h
-
-
-#endif /* forests_Bridging_Header_h */
-
 
 #import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
 #import <Bolts/BFTask.h>
-
--- forests/forests/ViewController.swift
+++ forests/forests/ViewController.swift
@@ -1,25 +1,23 @@
 //
 //  ViewController.swift
-//  forests
+//  ParseTutorial
 //
-//  Created by Sujay Patwardhan on 9/12/15.
-//  Copyright © 2015 Sujay Patwardhan. All rights reserved.
+//  Copyright (c) 2015 bizzi-body. All rights reserved.
 //
 
 import UIKit
-import Parse
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
