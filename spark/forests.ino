5506643efe79bb9f50926d6a7a87fc51fba03aa2
Pranay Prakash <pranay.gp@gmail.com>
2015-09-12 18:21:47 -0400

    Made a few changes to some files

--- spark/forests.ino
+++ spark/forests.ino
@@ -13,13 +13,31 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "TM1637.h"
+#include "HttpClient.h"
 #define CLK D2 //pins definitions for TM1637 and can be changed to other ports
 #define DIO D3
+#define LOGGING = 1
 TM1637 tm1637(CLK,DIO);
+
+unsigned int nextTime = 0;    // Next time to contact the server
+HttpClient http;
+
+// Headers currently need to be set at init, useful for API keys etc.
+http_header_t headers[] = {
+    //  { "Content-Type", "application/json" },
+    //  { "Accept" , "application/json" },
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+};
+
+http_request_t request;
+http_response_t response;
+
 void setup()
 {
   tm1637.init();
   tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  Serial.begin(9600);
 }
 void loop()
 {
@@ -39,6 +57,35 @@ void loop()
       i ++;
       if(i == sizeof(NumTab)) i = 0;
     }
+
+    if (nextTime > millis()) {
+        return;
+    }
+
+    Serial.println();
+    Serial.println("Application>\tStart of Loop.");
+    // Request path and body can be set at runtime or at setup.
+    /*request.hostname = "nHOps9WmG2ubPDRp1XA8gHR6YPB5fGDCAUjGUttN:javascript-key=sfvMLXaSvm2CUGsEbbLEc5HLXHp3vSr8EDlmteaU@api.parse.com/1/classes";
+    request.port = 80;
+    request.path = "BloombergObject";*/
+
+    request.hostname = "http://www.timeapi.org/";
+    request.port = 80;
+    request.path = "/utc/now";
+
+    // The library also supports sending a body with your request:
+    //request.body = "{\"key\":\"value\"}";
+
+    // Get request
+    http.get(request, response, headers);
+    Serial.print("Application>\tResponse status: ");
+    Serial.println(response.status);
+
+    Serial.print("Application>\tHTTP Response Body: ");
+    Serial.println(response.body);
+
+    nextTime = millis() + 10000;
+
     tm1637.display(0,ListDisp[0]);
     tm1637.display(1,ListDisp[1]);
     tm1637.display(2,ListDisp[2]);
