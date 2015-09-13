6a702853362bd5039cfd506949ca1f2d358f6c23
Sujay <cheeselover724@gmail.com>
2015-09-13 08:12:26 -0400

    Revert "UITableView rows working"
    This reverts commit d5c7c62fb7c7e3627e8638e1608c48412e565e02.

--- forests/forests/TableViewController.swift
+++ forests/forests/TableViewController.swift
@@ -19,55 +19,80 @@ class TableViewController: PFQueryTableViewController {
 		super.init(coder: aDecoder)
   
 		// Configure the PFQueryTableView
-//		self.parseClassName = "BloombergObject"
-//		self.textKey = "Ticker"
-//		self.pullToRefreshEnabled = false
-//		self.paginationEnabled = false
+		self.parseClassName = "BloombergObject"
+		self.textKey = "Ticker"
+		self.pullToRefreshEnabled = false
+		self.paginationEnabled = false
 	}
     
     
-//    var arr : NSMutableArray = []
+    var arr : NSMutableArray = []
     
-    override func viewWillAppear(animated: Bool) {
-        loadObjects()
-    }
-    
-    override func queryForTable() -> PFQuery {
-        let query = BloombergObject.query()
-        return query!
-    }
 	
-
+	// Define the query that will provide the data for the table view
+	override func queryForTable() -> PFQuery {
+        let query:PFQuery = PFQuery(className: "BloombergObject")
+		query.orderByAscending("Ticker")
+        print(query)
+        print ("sdfilbfblirdneirnbrevdvsfdbrb")
+        print(query.getObjectWithId("Ticker"))
+        query.findObjectsInBackgroundWithBlock{ (objects,error) -> Void in
+            if error == nil {
+                for object in objects! {
+                    self.arr.addObject(object)
+                }
+            }
+        }
+        return query
+	}
+    
 	//override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject!) -> PFTableViewCell? {
-        // 1
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! TableViewCell
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell {
+		
+        var cell = PFTableViewCell()
         
-        // 2
-        let bloomEntry = object as! BloombergObject
+
+            print("c,vzxbcv,mnzcxbv,mnbzxc,mvnbcx,mnvbxzc,mvbxzc,mnvb")
+            cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! PFTableViewCell!
+            if (cell as UITableViewCell? == nil) {
+                cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+            }
+            //        cell.textLabel!.text = "usdj"
+            
+            // Extract values from the PFObject to display in the table cell
+            if let ID = queryForTable().valueForKey("Ticker") as? String {
+                cell.textLabel!.text = ID
+                print("id" + ID)
+            }
         
         
-        // 4
-        let name = bloomEntry.Name
         
-        print(name)
+//        if let displayIntake = PFUser.currentUser()!["IntakeCode"] as? String {
+//            txtIntake.text = displayIntake
+//        }
         
-        cell.textLabel!.text = "\(name)"
+//        let name = object!.valueForKey("ObjectId") as! String
+//        print(name)
         
-        return cell
-    }
+//        print(object!.valueForKey("ObjectId") as! String)
+//		if let capital = object?["capital"] as? String {
+//			cell?.detailTextLabel?.text = capital
+//		}
+		
+		return cell
+	}
 	
 	// In a storyboard-based application, you will often want to do a little preparation before navigation
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		
 		// Get the new view controller using [segue destinationViewController].
-//		let detailScene = segue.destinationViewController as? DetailViewController
-//		
-//		// Pass the selected object to the destination view controller.
-//		if let indexPath = self.tableView.indexPathForSelectedRow {
-//			let row = Int(indexPath.row)
-//			detailScene?.currentObject = (objects?[row] as! PFObject)
-//		}
+		let detailScene = segue.destinationViewController as? DetailViewController
+		
+		// Pass the selected object to the destination view controller.
+		if let indexPath = self.tableView.indexPathForSelectedRow {
+			let row = Int(indexPath.row)
+			detailScene?.currentObject = (objects?[row] as! PFObject)
+		}
 	}
 	
 	override func viewDidAppear(animated: Bool) {
