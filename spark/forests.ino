db066483ddddac1a67c9a7e239f3ef72a7beaa29
Pranay Prakash <pranay.gp@gmail.com>
2015-09-13 02:14:43 -0400

    made spark work

--- spark/forests.ino
+++ spark/forests.ino
@@ -1,58 +1,66 @@
-//  Author:Frankie.Chu
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#include "application.h"
+#include "HttpClient.h"
 #include "TM1637.h"
-#include <string.h>
 
-#define CLK D2 //pins definitions for TM1637 and can be changed to other ports
-#define DIO D3
-
-TM1637 tm1637(CLK,DIO);
-
+/**
+* Declaring the variables.
+*/
 unsigned int nextTime = 0;    // Next time to contact the server
-TCPClient client;
+#define CLK D2//pins definitions for TM1637 and can be changed to other ports
+#define DIO D3
+TM1637 tm1637(CLK,DIO);
+HttpClient http;
 
-void setup()
-{
-  tm1637.init();
-  tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+// Headers currently need to be set at init, useful for API keys etc.
+http_header_t headers[] = {
+    //  { "Content-Type", "application/json" },
+    //  { "Accept" , "application/json" },
+    { "Accept" , "*/*"},
+    { NULL, NULL } // NOTE: Always terminate headers will NULL
+};
 
-  client.connect("http://www.timeapi.org/utc/now", 80);
+http_request_t request;
+http_response_t response;
+int tracker;
+
+void setup() {
+    Serial.begin(9600);
+    tracker = 13;
+
+    tm1637.init();
+    tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
 }
-void loop()
-{
-  int8_t NumTab[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//0~9,A,b,C,d,E,F
+
+void loop() {
+  int8_t NumTab[] = {0,1,2,3,4,5,6,7,8,9};
   int8_t ListDisp[4];
-  unsigned char i = 0;
-  unsigned char count = 0;
-  delay(150);
-  while(1)
-  {
-    i = count;
-    count ++;
-    if(count == sizeof(NumTab)) count = 0;
-    for(unsigned char BitSelect = 0;BitSelect < 4;BitSelect ++)
-    {
-      ListDisp[BitSelect] = NumTab[i];
-      i ++;
-      if(i == sizeof(NumTab)) i = 0;
+    if (nextTime > millis()) {
+        return;
     }
 
-    tm1637.display(0,ListDisp[0]);
-    tm1637.display(1,ListDisp[1]);
-    tm1637.display(2,ListDisp[2]);
-    tm1637.display(3,ListDisp[3]);
-    delay(300);
-  }
+    Serial.println();
+    Serial.println("Application>\tStart of Loop.");
+    // Request path and body can be set at runtime or at setup.
+    request.hostname = "secret-fjord-5332.herokuapp.com";
+    request.port = 80;
+    request.path = "/2011/ACRDBT.json";
+
+    // The library also supports sending a body with your request:
+    //request.body = "{\"key\":\"value\"}";
+
+    // Get request
+    http.get(request, response, headers);
+    char buff[5];
+    memcpy(buff, &response.body[tracker], 4);
+    Serial.println(buff);
+
+    tm1637.display(0,buff[0] - '0');
+    tm1637.display(1,buff[1] - '0');
+    tm1637.display(2,buff[2] - '0');
+    tm1637.display(3,buff[3] - '0');
+
+    nextTime = millis() + 5000;
+    tracker += 66;
+    if(tracker >= 400)
+      tracker = 13;
 }
